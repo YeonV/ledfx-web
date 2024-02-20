0dcf3dfd962a01cc67b160f14fec56661a9c9673
YeonV <trusted.yeon@gmail.com>
2024-02-20 23:25:56 +0100

    Adjust TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,10 +5,10 @@ import { getMobileOperatingSystem } from './utils'
 
 function TopBar({
   mode,
+  version,
   setMode
 }: // releases,
 // mirror,
-// version,
 // setMirror,
 // setVersion
 {
@@ -25,8 +25,8 @@ function TopBar({
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='fixed'>
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <Typography variant='h6'>Download LedFx</Typography>
+        <Toolbar sx={{ justifyContent: 'space-between', width: '100%', maxWidth: 1240, margin: '0 auto' }}>
+          <Typography variant='h6'>Download LedFx {version}</Typography>
           {!isAndroid && !isIOS && (
             <>
               <Stack direction={'row'} spacing={2} sx={{ color: '#fff' }} alignItems={'center'}>
